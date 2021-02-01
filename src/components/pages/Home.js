4cf3b5eb693f60049f4f25acbc3b2de6663af6ed
tarek-elmasry <dr.tareqelmasry@hotmail.com>
2021-02-01 21:23:43 +0300

    changing header at home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,39 +1,41 @@
-import { makeStyles , useMediaQuery, useTheme } from '@material-ui/core'
+import { makeStyles, Typography, useMediaQuery, useTheme } from '@material-ui/core'
 import React from 'react'
-import Carousel from 'react-material-ui-carousel'
-
-import img1 from '../../assets/pexels-chokniti-khongchum-3938022.jpg'
-import img2 from '../../assets/pexels-edward-jenner-4033148.jpg'
-import img3 from '../../assets/pexels-thirdman-5922103.jpg'
+import headerImg from '../../assets/header.jpg'
 
 
 const useStyles= makeStyles(theme => ({
   root: {
-    
+    padding: `0 ${theme.spacing(1)}px`,
+
   },
-  carouselImg: {
+  headerImg: {
     width: '100%',
     maxHeight: 650
+  },
+  headerContent: {
+    position: 'absolute',
+    top: '20%',
+    left: '20%',
+    backgroundColor: 'rgba(38, 20, 128,.8)',
+    color: 'white',
+    padding: theme.spacing(2),
+    background: 'lightblue'
   }
 }))
 function Home() {
   const classes= useStyles();
   const theme = useTheme()
-  const isMobile= useMediaQuery(theme.breakpoints.down('sm'));
-  const pic1 = img1
-  const pic2 = img2
-  const pic3= img3
+  const isMobile= useMediaQuery(theme.breakpoints.down('xs'));
+  
   return (
-    <div>
+    <div className={classes.root}>
       <div style={{position: 'relative'}}>
-        <Carousel navButtonsAlwaysVisible={isMobile? false : true} animation='slide'  timeout={{ enter: 500 , exit: 500}} >
-          <img src={pic1} alt='' className={classes.carouselImg}  />
-          <img src={pic2} alt='' className={classes.carouselImg}  />
-          <img src={pic3} alt='' className={classes.carouselImg}  />
-        </Carousel>
-      </div>
-      <div>
-        
+        <img src={headerImg} alt="" className={classes.headerImg}/>
+        <div className={classes.headerContent}>
+          <Typography style={{fontSize: isMobile ? '.8rem' : '2rem'}}>IBSA Regional Office</Typography>
+          <Typography style={{fontSize: isMobile ? '.8rem' : '2rem'}} gutterBottom>Gulf Area</Typography>
+          <Typography style={{fontSize: isMobile ? '.5rem' : '1rem'}}>Connecting People</Typography>
+        </div>
       </div>
     </div>
   )
